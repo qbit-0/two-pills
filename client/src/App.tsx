38e41b9c14dc86ea3f527d053c9d3c92e9ea208f
shin <sshoony95@gmail.com>
2023-02-05 14:28:41 -0500

    Add api to update a box given it's id

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,11 +5,21 @@ import './App.css'
 function App() {
   const [res, setRes] = useState(JSON);
   useEffect(() => {
-    const res = fetch("/api/boxes/link/1", {
-      method: 'Get',
+    // const res = fetch("/api/boxes/link/1", {
+    //   method: 'Get',
+    //   headers: {
+    //     'Content-type': 'application/json'
+    //   },
+    const data = {
+      link: "a link",
+      label: "a label"
+    }
+    const res = fetch("/api/boxes/1", {
+      method: 'PUT',
       headers: {
         'Content-type': 'application/json'
       },
+      body: JSON.stringify(data)
     }).then((res) => res.json())
     .then((data)=> setRes(data));
   },[])
--- server/routers/boxes.ts
+++ server/routers/boxes.ts
@@ -8,6 +8,13 @@ interface Box {
 };
 
 const boxes : Box[] = [{id:0, label: "First box", link: "F Link"}, {id:1, label: "Secend box", link: "S Link"}];
+router.put("/:id",async function (req, res, next) {
+  const id = Number(req.params.id);
+  boxes[id].label = await req.body.label;
+  boxes[id].link = await req.body.link;
+  console.log(req.body)
+  return res.status(200).json(boxes[id]);
+});
 
 /* GET label of all boxes. */
 router.get("/labels", function (req, res, next) {
