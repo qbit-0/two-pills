2ba3b9ba653af89e6f54da4ea9b70ffaca8ac1da
Duy Pham <duypham12241999@gmail.com>
2023-02-05 10:33:15 -0800

    Start over to practice

--- server/dist/routers/boxes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-    return res.status(200).json({ box1: { id: 1, lable: "First box" } });
-});
-module.exports = router;
--- server/dist/routers/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const boxesRouter = require('./boxes.js');
-const router = (0, express_1.Router)();
-router.use('/boxes', boxesRouter);
-module.exports = router;
