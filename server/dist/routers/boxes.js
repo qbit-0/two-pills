db6b83765d9ce267e4c96dfa63cf66050a1360f9
shin <sshoony95@gmail.com>
2023-02-07 09:55:19 -0500

    Update put method to handle error

--- server/dist/routers/boxes.js
+++ server/dist/routers/boxes.js
@@ -12,24 +12,33 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const router = (0, express_1.Router)();
 ;
-const boxes = [{ id: 0, label: "First box", link: "F Link" }, { id: 1, label: "Secend box", link: "S Link" }];
+const boxes = [
+    { label: "First box", link: "https://github.com/DuyAndShin/Two-Boxes" },
+    { label: "Secend box", link: "https://github.com/DuyAndShin" }
+];
+//const regex = new RegExp('https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)');
+const regex = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/);
+/* PUT a box. */
 router.put("/:id", function (req, res, next) {
     return __awaiter(this, void 0, void 0, function* () {
         const id = Number(req.params.id);
-        boxes[id].label = yield req.body.label;
-        boxes[id].link = yield req.body.link;
-        console.log(req.body);
-        return res.status(200).json(boxes[id]);
+        const label = req.body.label;
+        const link = req.body.link;
+        if (id >= boxes.length) {
+            return res.status(400).send({ error: "The index is out of scope" });
+        }
+        else if (!link.match(regex)) {
+            return res.status(400).send({ error: "The link is not valid" });
+        }
+        res.status(200).json(boxes[id].link);
+        boxes[id].label = label;
+        boxes[id].link = link;
+        return;
     });
 });
 /* GET label of all boxes. */
 router.get("/labels", function (req, res, next) {
     const labels = boxes.map((box) => box.label);
     return res.status(200).json(labels);
-});
-/* GET link. auth */
-router.get("/link/:id", function (req, res, next) {
-    const link = boxes[Number(req.params.id)].link;
-    return res.status(200).json(link);
 });
 module.exports = router;
