05d0a8ec44f26e0399f3ca7013d5205e3d0a80b6
shin <sshoony95@gmail.com>
2023-02-10 15:45:16 -0500

    Add a form

--- client/src/Popup.tsx
+++ client/src/Popup.tsx
@@ -1,5 +1,5 @@
 import "@/Popup.css";
-import { useEffect } from "react";
+import { useState } from "react";
 
 type PopupProps = {
   setPopupOpen: any;
@@ -7,32 +7,49 @@ type PopupProps = {
 };
 
 const Popup = ({ setPopupOpen, id }: PopupProps) => {
+  const [ link, setLink ] = useState<string>();
+  const [ label, setLabel ] = useState<string>()
+
   const handleSubmit = () => {
     setPopupOpen(false);
-  };
-
-  /* this need to be removed and
-      request has te be made upon submition of user input */
-  useEffect(() => {
     fetch(`/api/boxes/${id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        link: "https://developer.mozilla.org/en-US/",
-        label: "Study hard"
+        link,
+        label,
       }),
     })
       .then((res) => res.json())
       .then((data) => console.log(data));
-  }, []);
+  };
+  const handleCancel = () => {
+    setPopupOpen(false);
+  };
 
   return (
     <div className="popup">
-      <p>{id}</p>
-      <form method="dialog">
-        <button onClick={handleSubmit}>OK</button>
+      <h1>Provide a label and a link for the next person</h1>
+      <h2>You will get the current link after you submit the form</h2>
+      <form className="dialog">
+        <label>Label: </label>
+        <input
+          type="text"
+          name="label"
+          id="label"
+          placeholder="My favorite search engine"
+        />
+        <label>Link: </label>
+        <input
+          type="text"
+          name="link"
+          id="link"
+          placeholder="https://www.google.com/"
+        />
+        <button onClick={handleSubmit}>Submit</button>
+        <button onClick={handleCancel}>Cancel</button>
       </form>
     </div>
   );
