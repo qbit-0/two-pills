f71aa6a1bf2ce18f1bc6d25ee0c96fb3f71fd90f
Duy Pham <duypham12241999@gmail.com>
2023-02-12 20:46:51 -0800

    update backend with mogodb

--- server/src/models/pill.ts
+++ server/src/models/pill.ts
@@ -1,8 +1,29 @@
-type Pill = {
-  url: string;
-  label: string;
-  pickCount: number;
-  replaceCount: number;
-};
+import mongoose from "mongoose";
+
+export const pillSchema = new mongoose.Schema({
+  pillId: {
+    type: Number,
+    unique: true,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+  pickCount: {
+    type: Number,
+    required: true,
+  },
+  replaceCount: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Pill = mongoose.model("Pill", pillSchema);
 
 export default Pill;
